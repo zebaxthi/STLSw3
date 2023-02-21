ee9cf7ada1391512b8e03352e7a1e250b0b8f48a
estebanpoly1 <estebanpoly.e.a@gmail.com>
2023-02-20 21:16:17 -0500

    base

---
+++ stloan/src/main/java/com/uco/stloan/StloanApplication.java
@@ -0,0 +1,13 @@
+package com.uco.stloan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StloanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StloanApplication.class, args);
+	}
+
+}
---
+++ stloan/src/test/java/com/uco/stloan/StloanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uco.stloan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StloanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
