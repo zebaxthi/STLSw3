e75bfaa9f56cb91f8a09adda1b77e631e0b8f0ed
estebanpoly1 <estebanpoly.e.a@gmail.com>
2023-02-20 22:06:07 -0500

    domainArticulo

--- stloan/src/main/java/com/uco/stloan/domain/articulo/Articulo.java
+++ stloan/src/main/java/com/uco/stloan/domain/articulo/Articulo.java
@@ -1,6 +1,20 @@
 package com.uco.stloan.domain.articulo;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Articulo {
 
+    private Long id;
+    private String ref;
+    private String nombre;
+    private int cantidad;
 
+    public Articulo ( String ref, String nombre, int cantidad ) {
+        this.ref = ref;
+        this.nombre = nombre;
+        this.cantidad = cantidad;
+    }
 }
