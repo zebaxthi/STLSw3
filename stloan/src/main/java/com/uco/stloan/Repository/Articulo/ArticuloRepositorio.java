11de578c86793a942f83ae33f9ab3fa3db837ef0
Andres Jair Castillo Paredes <aj.faru@gmail.com>
2023-03-06 01:22:00 -0500

    prueba

--- stloan/src/main/java/com/uco/stloan/Repository/Articulo/ArticuloRepositorio.java
+++ stloan/src/main/java/com/uco/stloan/Repository/Articulo/ArticuloRepositorio.java
@@ -1,7 +1,8 @@
 package com.uco.stloan.Repository.Articulo;
 
 import com.uco.stloan.model.articulo.Articulo;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
-public interface ArticuloRepositorio extends CrudRepository<Articulo,String> {
+public interface ArticuloRepositorio extends JpaRepository<Articulo,String> {
 }
--- stloan/src/main/java/com/uco/stloan/Repository/Persona/PersonaRepositorio.java
+++ stloan/src/main/java/com/uco/stloan/Repository/Persona/PersonaRepositorio.java
@@ -1,7 +1,8 @@
 package com.uco.stloan.Repository.Persona;
 
 import com.uco.stloan.model.persona.Persona;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
-public interface PersonaRepositorio extends CrudRepository <Persona, String> {
+public interface PersonaRepositorio extends JpaRepository<Persona, String> {
 }
